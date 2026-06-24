b94773267398185934f8e749f961e2e527456fd2
Franco Giacobbe <frandg47@gmail.com>
2026-06-24 23:36:02 +0000

    security: move API URL to environment variable and add .env setup

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,5 +1,24 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://gio-backend.onrender.com/"
-})
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token");
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
